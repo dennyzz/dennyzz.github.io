0c96c724b99e2a257fda58485219ba6fd4deb222
Denny Zhang <dennyz@umich.edu>
2016-10-24 14:04:53 -0400

    trying a pythong script to extract image sizes

---
+++ imagesize.py
@@ -0,0 +1,69 @@
+# maybe be able to automate the image thing and also call up image sizes to the database to use with photoswipe
+import struct
+import imghdr
+
+def get_image_size(fname):
+    #'''Determine the image type of fhandle and return its size.
+    #from draco'''
+    with open(fname, 'rb') as fhandle:
+        head = fhandle.read(24)
+        if len(head) != 24:
+            return
+        if imghdr.what(fname) == 'png':
+            check = struct.unpack('>i', head[4:8])[0]
+            if check != 0x0d0a1a0a:
+                return
+            width, height = struct.unpack('>ii', head[16:24])
+        elif imghdr.what(fname) == 'gif':
+            width, height = struct.unpack('<HH', head[6:10])
+        elif imghdr.what(fname) == 'jpeg':
+            try:
+                fhandle.seek(0) # Read 0xff next
+                size = 2
+                ftype = 0
+                while not 0xc0 <= ftype <= 0xcf:
+                    fhandle.seek(size, 1)
+                    byte = fhandle.read(1)
+                    while ord(byte) == 0xff:
+                        byte = fhandle.read(1)
+                    ftype = ord(byte)
+                    size = struct.unpack('>H', fhandle.read(2))[0] - 2
+                # We are at a SOFn block
+                fhandle.seek(1, 1)  # Skip `precision' byte.
+                height, width = struct.unpack('>HH', fhandle.read(4))
+            except Exception: #IGNORE:W0703
+                return
+        else:
+            return
+        return width, height
+
+a,b = get_image_size("C:\Users\Denny\Dropbox\Public\Assets\Images\photography\\bungy\\thumbnails\\bungy-1.jpg")
+print a
+print b
+	
+
+# - id: stegosaurus
+#   description: Building a Snow Stegosaurus
+#   cover: IMG_3094.JPG
+#   imagefolder: https://dl.dropboxusercontent.com/u/19515841/Assets/Images/photography/stegosaurus/
+#   thumbfolder: https://dl.dropboxusercontent.com/u/19515841/Assets/Images/photography/stegosaurus/thumbnails/
+#   images:
+#   - name: IMG_3061.jpg
+#     thumb: IMG_3061.jpg
+#     text: Working Diligently
+#   - name: IMG_3073.jpg
+#     thumb: IMG_3073.jpg
+#     text: Denny and his Stegosaurus
+#   - name: IMG_3077.jpg
+#     thumb: IMG_3077.jpg
+#     text: Denny and Andrew
+#   - name: IMG_3080.jpg
+#     thumb: IMG_3080.jpg
+#     text: Denny and Akansha
+#   - name: IMG_3088.jpg
+#     thumb: IMG_3088.jpg
+#     text: Akansha and Andrew
+#   - name: IMG_3094.jpg
+#     thumb: IMG_3094.jpg
+#     text: All of us 
+
